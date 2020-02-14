a205f651f7de2ca8b8dc2f67da805debfe8513b6
AhmedEhabH <ahmed.ehab.h@gmail.com>
2020-02-14 20:19:58 +0200

    Comments

--- routes/web.php
+++ routes/web.php
@@ -21,18 +21,21 @@ Route::get('/', function () {
 
 Route::get('todos', 'TodosController@index');
 
+# Show Task
 Route::get('todos/{todo}', 'TodosController@show');
 
+# Create Task
 Route::get('new-todos', 'TodosController@create');
 
 Route::post('store-todos', 'TodosController@store');
 
-
+# Edit - Update
 Route::get('todos/{todo}/edit', 'TodosController@edit');
 
 Route::post('todos/{todo}/update-todos', 'TodosController@update');
 
-
+# Delete
 Route::get('todos/{todo}/delete', 'TodosController@delete');
 
+# Task Done
 Route::get('todos/{todo}/complete', 'TodosController@complete');
